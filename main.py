e027da4139cbcc12a5c2e70b70092fa657f945d8
Craig Fisk <craigfisk@marmalade.ai>
2025-10-13 16:01:42 -0400

    Added 2nd album. End of #3.

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ BANDS = [
     {"id": 1, 'name': 'The Kinks', 'genre': 'Rock'},          
     {"id": 2, 'name': 'Aphex Twin', 'genre': 'Electronic'},
     {"id": 3, 'name': 'Black Sabbath', 'genre': 'Metal', 'albums': [
-        {'title': 'Master of Reality', 'release_date': '1971-07-21'}
+        {'title': 'Master of Reality', 'release_date': '1971-07-21'},
+        {'title': 'Dog Food', 'release_date': '1972-07-21'}
     ]}, 
     {"id": 4, 'name': 'Wu-Tang Clan', 'genre': 'Hip-Hop'},    
 ]
