1fae30c0db6503f3541cfe7e53ba23d4b437b0f9
Craig Fisk <craigfisk@marmalade.ai>
2025-10-12 21:50:55 -0400

    In #2, changed return type from "str" (which gives error) to "dict" in @app.get("/bands/{band_id}").

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+BANDS = [
+    {"id": 1, 'name': 'The Kinks', 'genre': 'Rock'},          
+    {"id": 2, 'name': 'Aphex Twin', 'genre': 'Electronic'},
+    {"id": 3, 'name': 'Slowdive', 'genre': 'Shoegaze'}, 
+    {"id": 4, 'name': 'Wu-Tang Clan', 'genre': 'Hip-Hop'},    
+]
 
-@app.get('/')
-async def index() -> dict[str, str]:
-    return {'hello': 'world'}
-@app.get('/about')  
-async def about() -> str:
-    return {'An exceptional company.'}
+@app.get('/bands')
+async def bands() -> list[dict]:
+    return BANDS
+
+@app.get('/bands/{band_id}')  
+async def band(band_id: int) -> dict:
+    band = next((b for b in BANDS if b['id'] == band_id), None)
+    if band is None:
+        raise HTTPException(status_code=404, detail='Band not found')
+    return band
