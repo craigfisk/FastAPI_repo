1efb90a3de3f46c83709b5d80ad2584d7bae7e76
Craig Fisk <craigfisk@marmalade.ai>
2025-10-14 17:43:32 -0400

    In #5, added POST.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from schemas import GenreURLChoices, Band
+from schemas import GenreURLChoices, BandBase, BandCreate, BandWithID
 
 app = FastAPI()
 
@@ -17,8 +17,8 @@ BANDS = [
 async def bands(
     genre: GenreURLChoices | None = None,
     has_albums: bool = False
-    ) -> list[Band]:
-    band_list = [Band(**b) for b in BANDS]
+    ) -> list[BandWithID]:
+    band_list = [BandWithID(**b) for b in BANDS]
 
     if genre:
         band_list = [
@@ -30,14 +30,30 @@ async def bands(
     return band_list 
 
 @app.get('/bands/{band_id}')  
-async def band(band_id: int) -> Band:
-    band = next((Band(**b) for b in BANDS if b['id'] == band_id), None)
+async def band(band_id: int) -> BandWithID:
+    band = next((BandWithID(**b) for b in BANDS if b['id'] == band_id), None)
     if band is None:
         raise HTTPException(status_code=404, detail='Band not found')
     return band
 
-# @app.get('/bands/genre/{genre}')
-# async def bands_for_genre(genre: GenreURLChoices) -> list[dict]:
-#     return [
-#         b for b in BANDS if b['genre'].lower() == genre.value
-#     ]
+@app.get('/bands/genre/{genre}')
+async def bands_for_genre(genre: GenreURLChoices) -> list[dict]:
+    return [
+        b for b in BANDS if b['genre'].lower() == genre.value
+    ]
+
+@app.post("/bands")
+async def create_band(band_data: BandCreate) -> BandWithID:
+    id = BANDS[-1]['id'] + 1
+    band = BandWithID(id=id, **band_data.model_dump()).model_dump()
+    BANDS.append(band)
+    return BandWithID(**band)   
+    
+    # new_band = band_data.dict()
+    # new_band['id'] = new_id
+    # new_band['albums'] = []
+    # BANDS.append(new_band)
+    # return BandWithID(**new_band)
+    
+
+
