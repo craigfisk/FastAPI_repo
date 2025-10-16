323b1ecfd111f387d795bbfc208a172c4ed9ee98
Craig Fisk <craigfisk@marmalade.ai>
2025-10-15 21:58:27 -0400

    Not clear that it is working at end of #5. Issue is "@validator" has been deprecated and not clear how to write @field_validator(). It does seem (https://stackoverflow.com/questions/76734333/pydantic-v2-field-validator-values-argument-equivalent) that @validator (V1) is replaced by @field_validator (V@). Google on "field_validator" for more info.

--- schemas.py
+++ schemas.py
@@ -1,13 +1,19 @@
 from datetime import date
 
 from enum import Enum
-from pydantic import BaseModel
-
+from pydantic import BaseModel, field_validator
+ 
 class GenreURLChoices(Enum):
     ROCK = 'rock'
     ELECTRONIC = 'electronic'
     METAL = 'metal'
     HIP_HOP = 'hip-hop' 
+
+class GenreChoices(Enum):
+    ROCK = 'Rock'
+    ELECTRONIC = 'Electronic'
+    METAL = 'Metal'
+    HIP_HOP = 'Hip-hop' 
 
 # class Band(BaseModel):
 #     #     {"id": 1, 'name': 'The Kinks', 'genre': 'Rock'},  
@@ -18,12 +24,22 @@ class GenreURLChoices(Enum):
 
 class BandBase(BaseModel):
     name: str
-    genre: str
+    genre: GenreChoices
     albums: list['Album'] = []
 
 class BandCreate(BandBase):
-    pass
-
+    # @validator('genre', pre=True)
+    @field_validator('genre')
+    def genres_match(cls, v):
+        if isinstance(v, str):
+            v = v.strip().lower()
+            for genre in GenreChoices:
+                if genre.value.lower() == v:
+                    return genre
+        elif isinstance(v, GenreChoices):
+            return v
+        raise ValueError(f'Genre must be one of: {", ".join([g.value for g in GenreChoices])}')
+          
 class BandWithID(BandBase):
     id: int
 
